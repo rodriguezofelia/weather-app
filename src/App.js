29d62014d737e35d34847c4bb2c8fd08d1f822a9
Ofelia <oer.rodriguez@gmail.com>
2022-12-15 16:38:39 -0600

    updated logic city rendering and displaying on page

--- src/App.js
+++ src/App.js
@@ -1,51 +1,28 @@
 import "./App.css";
 import React from "react";
 import UserWeather from "./components/UserWeather";
-import BeachCities from "./components/BeachCities";
+import RenderCityData from "./components/Cities";
 
 export default function App() {
   return (
     <div className="App">
-      <p>Hi</p>
       {/* <UserWeather /> */}
-      <BeachCities />
+      <div>
+        <h1 className="cities"> Beach Cities</h1>
+        <p>
+          Looking to travel somewhere warm? Check out these cities and their
+          current conditions.
+        </p>
+      </div>
+      <RenderCityData locationType={"beachCities"} />
+      <div>
+        <h1 className="cities"> Ski Cities</h1>
+        <p>
+          Looking to travel somewhere cold? Check out these cities and their
+          current conditions.
+        </p>
+      </div>
+      <RenderCityData locationType={"skiCities"} />
     </div>
   );
 }
-
-// import "./App.css";
-// import React, { useEffect, useState } from "react";
-// import { WEATHER_APP_API_KEY, WEATHER_APP_API_URL } from "./api";
-// import UserWeather from "./components/UserWeather";
-
-// export default function App() {
-//   const [lat, setLat] = useState([]);
-//   const [long, setLong] = useState([]);
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       navigator.geolocation.getCurrentPosition(function (position) {
-//         setLat(position.coords.latitude);
-//         setLong(position.coords.longitude);
-//       });
-
-//       const response = await fetch(
-//         `${WEATHER_APP_API_URL}?lat=${lat}&lon=${long}&units=imperial&appid=${WEATHER_APP_API_KEY}`
-//       );
-//       const data = await response.json();
-//       // console.log(data, "DATAS");
-//       setData(data);
-//     };
-//     fetchData();
-//   }, [lat, long]);
-//   return (
-//     <div className="App">
-//       {typeof data != "undefined" ? (
-//         <UserWeather geoLocatedWeatherData={data} />
-//       ) : (
-//         <div>Geolocation Error</div>
-//       )}
-//     </div>
-//   );
-// }
---
+++ src/components/Cities.js
@@ -0,0 +1,107 @@
+import "../components/Cities.css";
+import React, { useState, useEffect } from "react";
+import data from "../data/data.json";
+import fetchWeatherData from "../fetchWeatherData";
+
+const RenderCityData = (props) => {
+  const [loading, setLoading] = useState(true);
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    const getWeather = async () => {
+      let returned_weather_data = [];
+      const getData = async (cityData) => {
+        let currentWeatherData = await fetchWeatherData(
+          cityData.lat,
+          cityData.lng
+        );
+        if (props.locationType === "beachCities") {
+          if (
+            currentWeatherData.current.temp >= 70 &&
+            currentWeatherData.current.wind_speed < 20 &&
+            currentWeatherData.current.clouds > 0
+          ) {
+            returned_weather_data.push({
+              name: cityData.name,
+              currentTemp: currentWeatherData.current.temp,
+              windSpeed: currentWeatherData.current.wind_speed,
+              hasClouds: currentWeatherData.current.clouds,
+              location: "beach",
+            });
+          } else {
+            returned_weather_data.push({
+              name: cityData.name,
+              currentTemp: currentWeatherData.current.temp,
+              windSpeed: currentWeatherData.current.wind_speed,
+              hasClouds: currentWeatherData.current.clouds,
+              alerts: currentWeatherData.alerts || "",
+              // COME BACK FIX DISQUALIFYING REASONS
+              disqualifyReason: `Reason(s) for disqualification: ${
+                currentWeatherData.current.temp ||
+                currentWeatherData.windSpeed ||
+                !!currentWeatherData.hasClouds ||
+                currentWeatherData.alerts
+              } `,
+            });
+          }
+        } else if (props.locationType === "skiCities") {
+          if (currentWeatherData.current.temp < 50) {
+            returned_weather_data.push({
+              name: cityData.name,
+              currentTemp: currentWeatherData.current.temp,
+              location: "ski",
+            });
+          } else {
+            returned_weather_data.push({
+              name: cityData.name,
+              currentTemp: currentWeatherData.current.temp,
+              alerts: currentWeatherData.alerts || "",
+              // COME BACK FIX DISQUALIFYING REASONS
+              disqualifyReason: `Reason(s) for disqualification: ${
+                currentWeatherData.current.temp || currentWeatherData.alerts
+              } `,
+            });
+          }
+        }
+      };
+      const locationType = props.locationType;
+      const promises = data[locationType].map((city) => getData(city));
+      await Promise.all(promises);
+      setLoading(false);
+      setWeatherData(returned_weather_data);
+    };
+    getWeather();
+  }, []);
+
+  return (
+    <div className="container">
+      {loading && weatherData.length === 0 ? (
+        <p>Loading data ...</p>
+      ) : (
+        <div>
+          <div className="header-section">
+            <div className="info">City</div>
+            <div className="info">Temperature</div>
+            <div className="info">Disqualified?</div>
+          </div>
+          {weatherData.map((city) => (
+            <div className="city-section">
+              {" "}
+              <div className="info">
+                <div>{city.name}</div>
+              </div>
+              <div className="info">
+                <div> {Math.trunc(city.currentTemp)}°</div>
+              </div>
+              <div className="info">
+                <div> {city.disqualifyReason}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default RenderCityData;
